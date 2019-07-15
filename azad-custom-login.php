8279f434ad93cf6e08d9b2b7f4ad744f8ead3688
Azad <azad@azad.com>
2019-07-15 22:05:12 +0600

    Initialized.

---
+++ azad-custom-login.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+Plugin Name: Azad Custom Login
+Description: Description will go here...
+Plugin URi: gittechs.com/plugin/azad-custom-login 
+Author: Md. Abul Kalam Azad
+Author URI: gittechs.com/author
+Author Email: webdevazad@gmail.com
+Version: 0.0.0.1
+Text Domain: azad-custom-login
+*/ 
+
+// EXIT IF ACCESSED DIRECTLY
+defined('ABSPATH') || exit;
